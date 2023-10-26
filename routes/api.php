f6bf8e9bef471018edace4f574b9c0b435320e15
Gilson96 <grafael99@gmail.com>
2023-10-26 11:03:44 +0100

    SPA Authentication

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(["prefix" => "restaurants"], function () {
+    // GET /restaurants: show all articles
+    Route::get("", [Restaurants::class, "index"]);
+    // POST /Restaurants: create a new article
+    Route::post("", [Restaurants::class, "store"]);
+    // all these routes also have an article ID in the
+    // end-point, e.g. /Restaurants/8
+    Route::group(["prefix" => "{restaurant}"], function () {
+        // GET /Restaurants/8: show the article
+        Route::get("", [Restaurants::class, "show"]);
+        // PUT /Restaurants/8: update the article
+        Route::put("", [Restaurants::class, "update"]);
+        // DELETE /Restaurants/8: delete the article
+        Route::delete("", [Restaurants::class, "destroy"]);
+    });
+});
