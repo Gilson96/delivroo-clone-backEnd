2a393a5a822d29bc2546004fd2a6d543473f3e1a
Gilson96 <grafael99@gmail.com>
2023-11-05 10:43:49 +0000

    Fix CRUD operations

--- app/Http/Requests/DishRequest.php
+++ app/Http/Requests/DishRequest.php
@@ -18,7 +18,7 @@ class DishRequest extends FormRequest
         return [
             "title" => ["required", "string", "max:100"],
             "description" => ["required", "string", "max:100"],
-            "price" => ["required", "decimal:10,2"],
+            "price" => ["required", "decimal:1,1"],
             "src" => ["required", "string", "max:100"]
         ];
     }
