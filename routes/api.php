19885832fa7b0b61daa4d3390afd7636c95da2d5
Gilson96 <grafael99@gmail.com>
2023-11-27 02:12:28 +0000

    Fix api routes

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,8 @@ Route::group(["prefix" => "restaurants"], function () {
 
         Route::group(["prefix" => "dishes"], function () {
             Route::get("", [RestaurantDishes::class, "index"]);
-            Route::post("", [RestaurantDishes::class, "store"]);          
+            Route::post("", [RestaurantDishes::class, "store"]);
+            Route::put("", [Dishes::class, "update"]);
         });
     });
 });
@@ -46,7 +47,7 @@ Route::group(["prefix" => "restaurants"], function () {
 Route::group(["prefix" => "dishes"], function () {
     Route::get("", [Dishes::class, "index"]);     
     Route::group(["prefix" => "{dish}"], function () {
-        Route::put("", [Dishes::class, "update"]);
+        
         Route::delete("", [Dishes::class, "destroy"]);
     });  
 });
