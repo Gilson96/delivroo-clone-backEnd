cef65ded7c8aa6b88c8f1a6c45f983091871baf6
Gilson96 <grafael99@gmail.com>
2024-01-11 23:04:29 +0000

    Changing front-end cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://gilson96.github.io/delivoo_clone-FrontEnd')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://gilson96.github.io
+    ')],
 
     'allowed_origins_patterns' => [],
 
